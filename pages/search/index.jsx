00e0b6342baefdf1b93e9ec31f1e940aba04005f
NicolasCouto <nicolas.couto@cazco.digital>
2023-03-30 18:20:20 -0300

    update: algolia search adjusted

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -1,81 +1,37 @@
-import { findResultsState } from "react-instantsearch-dom/server";
-import qs from "qs";
-import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/router";
-import algoliasearch from "algoliasearch/lite";
+import "react-toastify/dist/ReactToastify.min.css";
+import { useLang } from "../../Context/LangContext";
+
+import apiUnlogged from "../../services/apiUnlogged";
 
 import dynamic from "next/dynamic";
-
 const SearchMicro = dynamic(() => import("searchPage/searchPage"), {
   ssr: false,
 });
 
-const searchClient = algoliasearch(
-  process.env.NEXT_PUBLIC_REACT_APP_ALGOLIA_APP_ID,
-  process.env.NEXT_PUBLIC_REACT_APP_ALGOLIA_SEARCH_API_KEY
-);
+export default function Search(props) {
+  const { routeTranslations } = useLang();
+  const appAlgoliaIndexSearch =
+    process.env.NEXT_PUBLIC_REACT_APP_ALGOLIA_INDEX_SEARCH;
+  const mktName = process.env.NEXT_PUBLIC_REACT_APP_NAME;
+  const appImagesUrl = process.env.NEXT_PUBLIC_REACT_APP_IMAGES_URL;
+  const companyId = process.env.NEXT_PUBLIC_REACT_APP_COMPANY_ID;
 
-const updateAfter = 700;
-
-const createURL = (state) => `?${qs.stringify(state)}`;
-
-const pathToSearchState = (path) =>
-  path.includes("?") ? qs.parse(path.substring(path.indexOf("?") + 1)) : {};
-
-const searchStateToURL = (searchState) =>
-  searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : "";
-
-const DEFAULT_PROPS = {
-  searchClient,
-  indexName: process.env.NEXT_PUBLIC_REACT_APP_ALGOLIA_INDEX_SEARCH,
-};
-
-export default function SearchPage(props) {
-  const [searchState, setSearchState] = useState(props.searchState);
-  const router = useRouter();
-  const debouncedSetState = useRef();
-
-  useEffect(() => {
-    if (router) {
-      router.beforePopState(({ url }) => {
-        setSearchState(pathToSearchState(url));
-      });
-    }
-  }, [router]);
   return (
     <SearchMicro
-      {...DEFAULT_PROPS}
-      searchState={searchState}
-      resultsState={props.resultsState}
-      onSearchStateChange={(nextSearchState) => {
-        clearTimeout(debouncedSetState.current);
-
-        debouncedSetState.current = setTimeout(() => {
-          const href = searchStateToURL(nextSearchState);
-
-          router.push(href, href, { shallow: true });
-        }, updateAfter);
-
-        setSearchState(nextSearchState);
-      }}
-      createURL={createURL}
-      routeUrl={props.resolvedUrl}
+      apiUnlogged={apiUnlogged}
+      routeTranslations={routeTranslations}
+      appAlgoliaIndexSearch={appAlgoliaIndexSearch}
+      ssrData={props}
+      mktName={mktName}
+      appImagesUrl={appImagesUrl}
+      companyId={companyId}
     />
   );
 }
 
 export async function getServerSideProps({ resolvedUrl }) {
-  const searchState = pathToSearchState(resolvedUrl);
-
-  const resultsState = await findResultsState(SearchPage, {
-    ...DEFAULT_PROPS,
-    searchState,
-  });
-
   return {
     props: {
-      resultsState: JSON.parse(JSON.stringify(resultsState)),
-      searchState,
       resolvedUrl,
     },
   };
