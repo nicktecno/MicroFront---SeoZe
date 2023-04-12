6449c4380628fcf395251722e4e09737a69e6dc0
NicolasCouto <nicolas.couto@cazco.digital>
2023-04-12 13:56:38 -0300

    update: microfront category adjust

--- pages/category/[...term].jsx
+++ pages/category/[...term].jsx
@@ -1,29 +1,30 @@
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const SearchMicro = dynamic(() => import("searchPage/searchPage"), {
-  ssr: false,
-});
+// const SearchMicro = dynamic(() => import("searchPage/searchPage"), {
+//   ssr: false,
+// });
 
-import { useLang } from "../../Context/LangContext";
+// import { useLang } from "../../Context/LangContext";
 
-import apiUnlogged from "../../services/apiUnlogged";
+// import apiUnlogged from "../../services/apiUnlogged";
 
 export default function CategoryPage() {
-  const { routeTranslations } = useLang();
-  const appAlgoliaIndexSearch =
-    process.env.NEXT_PUBLIC_REACT_APP_ALGOLIA_INDEX_SEARCH;
-  const mktName = process.env.NEXT_PUBLIC_REACT_APP_NAME;
-  const appImagesUrl = process.env.NEXT_PUBLIC_REACT_APP_IMAGES_URL;
-  const companyId = process.env.NEXT_PUBLIC_REACT_APP_COMPANY_ID;
+  <></>;
+  // const { routeTranslations } = useLang();
+  // const appAlgoliaIndexSearch =
+  //   process.env.NEXT_PUBLIC_REACT_APP_ALGOLIA_INDEX_SEARCH;
+  // const mktName = process.env.NEXT_PUBLIC_REACT_APP_NAME;
+  // const appImagesUrl = process.env.NEXT_PUBLIC_REACT_APP_IMAGES_URL;
+  // const companyId = process.env.NEXT_PUBLIC_REACT_APP_COMPANY_ID;
 
-  return (
-    <SearchMicro
-      apiUnlogged={apiUnlogged}
-      routeTranslations={routeTranslations}
-      appAlgoliaIndexSearch={appAlgoliaIndexSearch}
-      mktName={mktName}
-      appImagesUrl={appImagesUrl}
-      companyId={companyId}
-    />
-  );
+  // return (
+  //   <SearchMicro
+  //     apiUnlogged={apiUnlogged}
+  //     routeTranslations={routeTranslations}
+  //     appAlgoliaIndexSearch={appAlgoliaIndexSearch}
+  //     mktName={mktName}
+  //     appImagesUrl={appImagesUrl}
+  //     companyId={companyId}
+  //   />
+  // );
 }
